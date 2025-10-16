e7f1c7af3f17288a134d97ce1d650e481e6a95ff
Kethellen <mendoncakethellen@gmail.com>
2025-10-16 16:19:10 -0300

    corrigindo success.js vazio

---
+++ pages/success.js
@@ -0,0 +1,8 @@
+export default function Success() {
+  return (
+    <main className="container py-5">
+      <h1>Pagamento concluído</h1>
+      <p>Obrigado! Sua compra foi processada. Em breve você receberá um e-mail com detalhes.</p>
+    </main>
+  );
+}
